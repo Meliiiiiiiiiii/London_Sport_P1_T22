d32f656cfbe0f86ac3aa6ef8766d704560b6875d
Shameta <mohammedshameem@Mohammeds-MacBook-Pro.local>
2026-07-31 18:30:27 +0100

    Remove misplaced files, will re-add inside locator APPLICATION folder

--- src/cognitoConfig.js
+++
@@ -1,6 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-export const userPool = new CognitoUserPool({
-  UserPoolId: "eu-north-1_3M1BWmGCa",
-  ClientId: "6la1uqbd6lm2m9hpf8223m4v23",
-});
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { C, FONT_MONO } from "../theme";
-
-export default function Footer() {
-  return (
-    <div style={{ textAlign: "center", padding: "24px 16px 30px", fontFamily: FONT_MONO, fontSize: 11, color: C.textMuted, letterSpacing: "0.04em" }}>
-      ACTIVITY DATA REFRESHED WEEKLY · SOURCE: OPENACTIVE
-    </div>
-  );
-}
